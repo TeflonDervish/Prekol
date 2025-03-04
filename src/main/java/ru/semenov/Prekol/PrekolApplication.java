54b248902ded235167904bbd5878708cbb0772e9
vsemenov <Vyacheslav.Semenov@slsoft.ru>
2025-03-04 17:16:54 +0500

    Initial commit

---
+++ src/main/java/ru/semenov/Prekol/PrekolApplication.java
@@ -0,0 +1,13 @@
+package ru.semenov.Prekol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrekolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrekolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/semenov/Prekol/PrekolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.semenov.Prekol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrekolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
